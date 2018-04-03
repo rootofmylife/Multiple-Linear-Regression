6106f8b825708b1c250ff28ffde6936f7c93e1fc
Duc Do <dotrananhduc@gmail.com>
2018-04-03 23:26:46 +0700

    update

---
+++ main.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+def computeCost(x_train1, x_train2, y_origin, b00, b11, b22): # loss funtion
+    sum = 0
+    for i in range(len(y_origin)):
+        sum += ((b00 + b11 * x_train1[i] / 10000 + b22 * x_train2[i] / 10) - y_origin[i])**2
+        
+    return sum / float((2 * len(y_origin)))
+
+def gradientDescent(x_train1, x_train2, y_origin, b0, b1, b2, learning_rate):
+    db0, db1, db2 = 0, 0, 0
+    for i in range(len(y_origin)):
+        # db0 += (b0 + b1 * (x_train1[i] - np.mean(x_train1[i])) / np.std(x_train1[i]) + b2 * (x_train2[i] - np.mean(x_train2[i])) / np.std(x_train2[i]) - y_origin[i])
+        # db1 += (b0 + b1 * (x_train1[i] - np.mean(x_train1[i])) / np.std(x_train1[i]) + b2 * (x_train2[i] - np.mean(x_train2[i])) / np.std(x_train2[i]) - y_origin[i]) * x_train1[i]
+        # db2 += (b0 + b1 * (x_train1[i] - np.mean(x_train1[i])) / np.std(x_train1[i]) + b2 * (x_train2[i] - np.mean(x_train2[i])) / np.std(x_train2[i]) - y_origin[i]) * x_train2[i]
+        db0 += (b0 + b1 * x_train1[i] / 10000 + b2 * x_train2[i] / 10 - y_origin[i])
+        db1 += (b0 + b1 * x_train1[i] / 10000 + b2 * x_train2[i] / 10 - y_origin[i]) * x_train1[i]
+        db2 += (b0 + b1 * x_train1[i] / 10000 + b2 * x_train2[i] / 10 - y_origin[i]) * x_train2[i]
+        
+    # computeGradient
+    db0 = db0 / float(len(y_origin))
+    db1 = db1 / float(len(y_origin))
+    db2 = db2 / float(len(y_origin))
+
+    b_0 = b0 - (learning_rate * db0)
+    b_1 = b1 - (learning_rate * db1)
+    b_2 = b2 - (learning_rate * db2)
+    return b_0, b_1, b_2
+
+if __name__ == '__main__':
+    with open('./ex1data2.txt') as f:
+        data = f.read().splitlines()
+
+    data = [s.split(',') for s in data]
+
+    dien_tich = [int(s[0]) for s in data] # x_train2
+    sl_phong_ngu = [int(s[1]) for s in data] # x_train1
+    price = [int(s[2]) for s in data] # y_value
+
+    b0, b1, b2 = 1000, 1644273, 3178
+    lr = 0.000005
+
+    # learning
+    print('1 st Loss: ' + str(computeCost(dien_tich,sl_phong_ngu, price, b0, b1, b2)))
+    for it in range(10000):
+        b0, b1, b2 = gradientDescent(dien_tich,sl_phong_ngu, price, b0, b1, b2, lr)
+            
+    print('Last Loss: ' + str(computeCost(dien_tich,sl_phong_ngu, price, b0, b1, b2)))
+    print('B0: ' + str(b0) + ' ' + 'B1: ' + str(b1) + ' ' + 'B2: ' + str(b2))
+
+    # Graph
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+
+
+    # X, Y = np.meshgrid(dien_tich, sl_phong_ngu)
+    # zs = np.array([fun(x,y) for x,y in zip(np.ravel(X), np.ravel(Y))])
+    # Z = zs.reshape(X.shape)
+
+    # ax.plot_surface()
+    # ax.scatter(dien_tich, sl_phong_ngu, price, c='r', marker='o')
+
+    # ax.set_xlabel('Area')
+    # ax.set_ylabel('Rooms')
+    # ax.set_zlabel('Prices')
+
+    # plt.show()
